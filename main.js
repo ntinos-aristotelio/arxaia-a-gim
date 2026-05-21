3739efc122cb34d92da6b40b70a19f58f1df90c3
ntinos-aristotelio <konstantinos@aristotelio.gr>
2026-05-21 12:09:50 +0300

    Delete main.js

--- main.js
+++
@@ -1,5 +0,0 @@
-function loadSection(id) {
-    const data = sectionsData[id];
-    document.getElementById('dynamic-content').innerHTML = `<h1>${data.title}</h1><ul>` + 
-        data.vocab.map(v => `<li>${v.ancient}: ${v.modern}</li>`).join('') + `</ul>`;
-}
