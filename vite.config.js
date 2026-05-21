d4a9b1d633541028104f9577c40e1d28eae71973
ntinos-aristotelio <konstantinos@aristotelio.gr>
2026-05-21 11:57:04 +0300

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,7 @@
 import { defineConfig } from 'vite'
-    import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react'
 
-    // https://vitejs.dev/config/
-    export default defineConfig({
-      plugins: [react()],
-      base: '/arxaia-a-gim/',
-    })
-    ```
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+})
